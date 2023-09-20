2198c570a007d2db152ec07f65e4f4ff706fb24d
copperthief <copperthiefcorvid@gmail.com>
2023-09-20 01:38:56 -0400

    first commit

---
+++ init.lua
@@ -0,0 +1,28 @@
+require "options"
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+	-- colorschemes
+	"savq/melange",
+	"ellisonleao/gruvbox.nvim",
+	"shaunsingh/nord.nvim",
+	"rebelot/kanagawa.nvim",
+	{ "bluz71/vim-nightfly-colors", name = "nightfly" },
+	"NLKNguyen/papercolor-theme",
+	"joshdick/onedark.vim",
+	"romainl/Apprentice"
+}
+
+require("lazy").setup(plugins, opts)
