2bd5ac0eb5e7fa1077abe9ac421302600d14a75f
copperthief <copperthiefcorvid@gmail.com>
2023-09-22 21:35:12 -0400

    moved plugins to seperate file

--- init.lua
+++ init.lua
@@ -1,38 +1,6 @@
+
+require "lava.plugins"
 require "lava.options"
 require "lava.currenttheme"    -- make it so if this doesn't work lua wont crash
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-local plugins = {
-	-- colorschemes
-	"savq/melange",
-	"ellisonleao/gruvbox.nvim",
-	"shaunsingh/nord.nvim",
-	"rebelot/kanagawa.nvim",
-	{ "bluz71/vim-nightfly-colors", name = "nightfly" },
-	"NLKNguyen/papercolor-theme",
-	"joshdick/onedark.vim",
-	"romainl/Apprentice",
-
-  {
-      'windwp/nvim-autopairs',
-      event = "InsertEnter",
-      opts = {} -- this is equalent to setup({}) function
-  },
-
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-}
-
-require("lazy").setup(plugins, opts)
 
---
+++ lua/lava/plugins.lua
@@ -0,0 +1,32 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  
+	-- colorschemes
+	"savq/melange",
+	"ellisonleao/gruvbox.nvim",
+	"shaunsingh/nord.nvim",
+	"rebelot/kanagawa.nvim",
+	{ "bluz71/vim-nightfly-colors", name = "nightfly" },
+	"NLKNguyen/papercolor-theme",
+	"joshdick/onedark.vim",
+	"romainl/Apprentice",
+
+  { "windwp/nvim-autopairs", event = "InsertEnter", opts = {} },
+
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+
+}
+
+require("lazy").setup(plugins, opts)
